870808d214a61c52a87a2002f4292a0542be2e4e
KangHaSeong <shaawwert6044@gmail.com>
2023-04-12 11:49:13 +0900

    페이지 리렌더링 시 goal input이 초기화되는 에러 수정

--- src/pages/Todo.js
+++ src/pages/Todo.js
@@ -1,5 +1,5 @@
 import TodoList from "../components/TodoList";
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import styled from "styled-components";
 import { AiOutlineEdit, AiOutlineCheckSquare } from 'react-icons/ai';
 
@@ -36,6 +36,14 @@ const TodoPage = () => {
   const [isEditing, setIsEditing] = useState(false);
   const [goalInputValue, setGoalInputValue] = useState("");
 
+  const goalInputRef = useRef(null); // useRef()를 사용해서 Goal Input의 ref를 생성합니다.
+
+  useEffect(() => { // useEffect() hook을 사용해서 Goal Input의 값을 유지합니다.
+    if (goalInputRef.current && goalInputRef.current.value !== goalInputValue) {
+      goalInputRef.current.value = goalInputValue;
+    }
+  }, [goalInputValue]);
+
   const handleEditClick = () => {
     setIsEditing(true);
   };
@@ -53,6 +61,7 @@ const TodoPage = () => {
           <TodoGoalInput
             type="text"
             placeholder="할 일의 우선순위를 파악할 수 있을만한 Core Standard를 적어주세요!"
+            ref={goalInputRef} // Goal Input의 ref를 설정합니다.
             value={goalInputValue}
             disabled={!isEditing}
             onChange={(e) => setGoalInputValue(e.target.value)}
@@ -75,3 +84,5 @@ const TodoPage = () => {
 };
 
 export default TodoPage;
+
+
